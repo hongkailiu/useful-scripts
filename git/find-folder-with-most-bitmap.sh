cba8fc013291938be7c9902bf91b4f08475e16a1
Hongkai Liu <hongkailiu@users.noreply.github.com>
2017-01-19 15:55:39 -0500

    Update find-folder-with-most-bitmap.sh

--- git/find-folder-with-most-bitmap.sh
+++ git/find-folder-with-most-bitmap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ref. http://unix.stackexchange.com/questions/111949/get-list-of-subdirectories-which-contain-a-file-matching-a-string
-find . -type f -name '*.bitmap^C| sed -r 's|/[^/]+$||' |sort |uniq
+find . -type f -name '*.bitmap' | sed -r 's|/[^/]+$||' |sort |uniq
