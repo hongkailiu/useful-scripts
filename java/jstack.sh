346ec18fdc2c7e178f369a16d84c98106195eea1
Hongkai Liu <hongkai.liu@ericsson.com>
2017-03-15 12:27:55 -0400

    Update jstack.sh

--- java/jstack.sh
+++ java/jstack.sh
@@ -2,20 +2,29 @@
 
 # http://howtodoinjava.com/for-fun-only/how-to-get-thread-dump-in-linux-using-jstack/
 
-if [ $# -eq 0 ]; then
-    echo >&2 "Usage: jstackSeries  [  [  ] ]"
-    echo >&2 "    Defaults: count = 10, delay = 1 (seconds)"
-    exit 1
+if [[ $# -eq 0 ]]; then
+  pid=$(jps | grep 'gerrit.war' | awk '{print $1}')
+else
+  pid=$1
 fi
 
-pid=$1          # required
 count=${2:-10}  # defaults to 10 times
 delay=${3:-1} # defaults to 1 second
 
+ssh -p 29418 emcgitsvc@localhost gerrit show-caches &
+
 while [ $count -gt 0 ]
 do
-    jstack $pid >jstack.$pid.$(date +%H%M%S.%N)
+    jstack $pid > jstack.result.$pid.$(date +%H%M%S.%N)
     sleep $delay
     let count--
     echo -n "."
 done
+
+echo " jstack DONE"
+
+# check the line about SshCommand
+grep -rni sshcommand ./jstack.result*
+
+
+echo " grep DONE"
